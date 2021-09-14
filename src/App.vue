fafdc53140a4c2e8dc61c88ee97142ba3c988521
vovaoecoyc <vovaoecoyc@yandex.ru>
2021-09-14 03:21:09 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <DrawArea/>
+</template>
+
+<script>
+import DrawArea from './components/DrawArea.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DrawArea
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/DrawArea.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="area" ref="area">
+
+  </div>
+</template>
+
+<script>
+  import { SVG } from '@svgdotjs/svg.js'
+
+  export default {
+    name: 'DrawArea',
+    mounted() {
+      this.rootSvgArea = SVG().addTo(this.$refs.area).size(150, 150);
+      this.rootSvgArea.rect(100, 100).attr({ fill: '#f06' });
+      setTimeout(() => {
+        this.rootSvgArea.clear();
+      }, 4000);
+    },
+    data: () => ({
+      rootSvgArea: null,
+    })
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
